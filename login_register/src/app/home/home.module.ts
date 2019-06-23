69f6cdf4e5291633afaff77aca24ec25829c4375
Paula <paula.bicca@gmail.com>
2019-06-22 22:59:30 -0300

    mudança de localidade (pt-br) - calendar

--- login_register/src/app/home/home.module.ts
+++ login_register/src/app/home/home.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgCalendarModule } from 'ionic2-calendar';
+import { NgModule, LOCALE_ID} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
+
+import localePT from '@angular/common/locales/pt';
+import localeExtraPT from '@angular/common/locales/extra/pt';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localePT, 'pt', localeExtraPT);
 
 const routes: Routes = [
   {
@@ -17,8 +23,11 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    NgCalendarModule,
     RouterModule.forChild(routes)
   ],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   declarations: [HomePage]
 })
+
 export class HomePageModule {}
